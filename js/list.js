4b87a73be690cb25f21d7016df1edebf57e8fa59
Vallejos Jose Luis <joseluisvallejos1995@gmail.com>
2022-11-22 00:32:32 -0300

    :construction: :bookmark: WIP, add task DONE and new tag.

---
+++ js/list.js
@@ -0,0 +1,31 @@
+const recibeTask = () => {
+  const taskInput = document.getElementById("taskInput");
+  const addTask = document.getElementById("addTask");
+  const taskList = [];
+
+  addTask.addEventListener("click", () => {
+    taskList.push(taskInput.value);
+    generatedList(taskList);
+    taskInput.value = "";
+  });
+
+};
+
+const generatedList = (taskList) => {
+  const taskContainer = document.getElementById("taskContainer");
+  let lastTask = `
+                <article class="task">
+                    <div class="tasks__container__task">
+                        <span class="tasks__container__task__circle"></span>
+                        <p class="tasks__container__task__text">${taskList[taskList.length - 1]}
+                        </p>
+                    </div>
+                    <picture>
+                        <img class="tasks__container__task__delete" src="./images/icon-cross.svg" alt="Icon Cross">
+                    </picture>
+                </article>
+                `;
+  taskContainer.insertAdjacentHTML("afterbegin", lastTask);
+};
+
+recibeTask();
