2bc50d493a652c97875df3e47e2055d9d811ed68
Vallejos Jose Luis <joseluisvallejos1995@gmail.com>
2022-12-10 17:22:40 -0300

    :hammer: Check tasks with local storage DONE.

--- js/checkTask.js
+++ js/checkTask.js
@@ -11,6 +11,19 @@ const checkTask = () => {
                 task.childNodes[1].classList.toggle("hide");
                 task.parentElement.classList.toggle("checkText");
                 task.classList.toggle("check");
+
+                console.log(task.parentElement.childNodes[3].textContent);
+
+                const allTasks = JSON.parse(localStorage.getItem("tasks"));
+
+                allTasks.forEach((taskLS) => {
+                    if (taskLS.text === task.parentElement.childNodes[3].textContent) {
+                        taskLS.checked = !taskLS.checked;
+
+                        localStorage.setItem("tasks", JSON.stringify(allTasks));
+                    }
+                }
+                );
             });
         }
         );
