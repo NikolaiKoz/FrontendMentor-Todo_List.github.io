b7823bc8e1436615f4246726bb193e6c95ae424b
Vallejos Jose Luis <joseluisvallejos1995@gmail.com>
2022-12-15 18:38:55 -0300

    :bug: fixed when loading a new task, DONE.

--- js/addNewTask.js
+++ js/addNewTask.js
@@ -1,44 +1,40 @@
 const addNewTask = () => {
+  const addTaskBtn = document.getElementById("addTask");
+  const inputTask = document.getElementById("taskInput");
+  const taskContainer = document.getElementById("taskContainer");
 
-    const addTaskBtn = document.getElementById('addTask');
-    const inputTask = document.getElementById('taskInput');
-    const taskContainer = document.getElementById('taskContainer');
+  const addTask = () => {
+    const tasks = JSON.parse(localStorage.getItem("tasks"));
+    let lastInput = tasks[tasks.length - 1];
 
-    const addTask = () => {
+    const taskCard = `
+        <article class="task mb-light">
+            <div class="tasks__container__task">
+                <span class="tasks__container__task__circle">
+                    <img class="imgPaloma hide" src="./images/icon-check.svg" alt="Check" class="checkSvg">
+                </span>
+                <div class="tasks__container__task text-light">
+                <p class="tasks__container__task__text">${lastInput.text}</p>
+                </div>
+            <picture>
+                <img class="tasks__container__task__delete" src="./images/icon-cross.svg" alt="Icon Cross">
+            </picture>
+        </article>
+`;
 
-        const tasks = JSON.parse(localStorage.getItem('tasks'));
-        let lastInput = tasks[tasks.length - 1];
-        console.log(lastInput);
+    taskContainer.insertAdjacentHTML("afterbegin", taskCard);
 
-        const taskCard =    `
-                                            <article class="task">
-                                                <div class="tasks__container__task">
-                                                    <span class="tasks__container__task__circle">
-                                                        <img class="imgPaloma hide" src="./images/icon-check.svg" alt="Check" class="checkSvg">
-                                                    </span>
-                                                    <p class="tasks__container__task__text">${lastInput.text}</p>
-                                                </div>
-                                                <picture>
-                                                    <img class="tasks__container__task__delete" src="./images/icon-cross.svg" alt="Icon Cross">
-                                                </picture>
-                                            </article>
-                            `;
+    counterTasks();
+  };
 
-        taskContainer.insertAdjacentHTML('afterbegin', taskCard);
+  addTaskBtn.addEventListener("click", () => {
+    addTask();
+  });
 
-        counterTasks();
-    };
-
-    addTaskBtn.addEventListener('click', () => {
-        addTask();
-    });
-
-    inputTask.addEventListener('keyup', (e) => {
-        if (e.key === 'Enter') {
-            addTask();
-        }
+  inputTask.addEventListener("keyup", (e) => {
+    if (e.key === "Enter") {
+      addTask();
     }
-    );
-
+  });
 };
 addNewTask();
