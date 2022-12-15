724f861c7e5c07a63e6dfdd2e4bcfb162ddf3700
Vallejos Jose Luis <joseluisvallejos1995@gmail.com>
2022-12-15 19:30:07 -0300

    :bookmark: CHALLENGE, DONE.

--- js/addNewTask.js
+++ js/addNewTask.js
@@ -7,20 +7,40 @@ const addNewTask = () => {
     const tasks = JSON.parse(localStorage.getItem("tasks"));
     let lastInput = tasks[tasks.length - 1];
 
-    const taskCard = `
-        <article class="task mb-light">
-            <div class="tasks__container__task">
-                <span class="tasks__container__task__circle">
-                    <img class="imgPaloma hide" src="./images/icon-check.svg" alt="Check" class="checkSvg">
-                </span>
-                <div class="tasks__container__task text-light">
-                <p class="tasks__container__task__text">${lastInput.text}</p>
-                </div>
-            <picture>
-                <img class="tasks__container__task__delete" src="./images/icon-cross.svg" alt="Icon Cross">
-            </picture>
-        </article>
+    let taskCard = "";
+
+    const darkSideBtn = document.getElementById("darkSide");
+    if (darkSideBtn.src.includes("sun")) {
+      taskCard = `
+      <article class="task bb-dark">
+          <div class="tasks__container__task">
+              <span class="tasks__container__task__circle">
+                  <img class="imgPaloma hide" src="./images/icon-check.svg" alt="Check" class="checkSvg">
+              </span>
+              <div class="tasks__container__task text-dark">
+              <p class="tasks__container__task__text">${lastInput.text}</p>
+              </div>
+          <picture>
+              <img class="tasks__container__task__delete" src="./images/icon-cross.svg" alt="Icon Cross">
+          </picture>
+      </article>
 `;
+    } else {
+      taskCard = `
+      <article class="task bb-light">
+          <div class="tasks__container__task">
+              <span class="tasks__container__task__circle">
+                  <img class="imgPaloma hide" src="./images/icon-check.svg" alt="Check" class="checkSvg">
+              </span>
+              <div class="tasks__container__task text-light">
+              <p class="tasks__container__task__text">${lastInput.text}</p>
+              </div>
+          <picture>
+              <img class="tasks__container__task__delete" src="./images/icon-cross.svg" alt="Icon Cross">
+          </picture>
+      </article>
+`;
+    }
 
     taskContainer.insertAdjacentHTML("afterbegin", taskCard);
 
