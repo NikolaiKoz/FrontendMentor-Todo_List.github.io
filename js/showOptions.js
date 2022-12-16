53af483f9708cacf800b1cc560adc8ef2e1bdec7
Vallejos Jose Luis <joseluisvallejos1995@gmail.com>
2022-12-15 21:56:21 -0300

    :hammer: showOptions.js, DONE.

--- js/showOptions.js
+++ js/showOptions.js
@@ -1,21 +1,27 @@
-
 const generatedOptions = () => {
+  const mobileContainer = document.getElementById("mobileContainer");
+  const desktopContainer = document.getElementById("desktopContainer");
 
-    const mobileContainer = document.getElementById('mobileContainer');
-    const desktopContainer = document.getElementById('desktopContainer');
-
-    const optionsToShow =   `
+  const optionsToShow = `
                                 <button id="allTasks" class="footer__main__btn select">All</button>
                                 <button id="activeTasks" class="footer__main__btn">Active</button>
                                 <button id="completedTasks" class="footer__main__btn">Completed</button>
                             `;
 
-    if (window.innerWidth < 768) {
-        mobileContainer.innerHTML += optionsToShow;
-    } else {
-        document.getElementById('mobileContainer').remove();
-        desktopContainer.innerHTML += optionsToShow;
-    }
+  if (window.innerWidth < 768) {
+    mobileContainer.innerHTML += optionsToShow;
+  } else {
+    document.getElementById("mobileContainer").remove();
+    desktopContainer.innerHTML += optionsToShow;
+  }
 
+  //Crear un lisener en la ventana para que cada vez que se cambie de tamaño se vuelva a ejecutar la funcion
+  window.addEventListener("resize", () => {
+    if (window.innerWidth > 768) {
+      window.location.reload();
+    } else {
+      window.location.reload();
+    }
+  });
 };
 generatedOptions();
