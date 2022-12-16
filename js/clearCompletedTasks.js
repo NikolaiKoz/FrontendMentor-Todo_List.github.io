a49c5114fd450912812cf268fb22001eaaca515c
Vallejos Jose Luis <joseluisvallejos1995@gmail.com>
2022-12-15 22:32:53 -0300

    :bug: Fixed the bug in clear completed, DONE.

--- js/clearCompletedTasks.js
+++ js/clearCompletedTasks.js
@@ -6,6 +6,14 @@ const clearCompletedTasks = () => {
         const task = document.querySelectorAll(".task");
         task.forEach((task) => {
             if (task.childNodes[1].childNodes[1].classList.contains("check")) {
+                const allTasks = JSON.parse(localStorage.getItem("tasks"));
+                allTasks.forEach((taskLS) => {
+                    if (taskLS.text === task.childNodes[1].childNodes[3].childNodes[1].textContent) {
+                        const index = allTasks.indexOf(taskLS);
+                        allTasks.splice(index, 1);
+                        localStorage.setItem("tasks", JSON.stringify(allTasks));
+                    }
+                });
                 task.remove();
             }
         });
